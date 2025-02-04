ccec822bf2dbcbb18fb8cfe4db2af0cf8e7bd364
Zheng Gong <hudenjear@foxmail.com>
2025-02-04 11:39:25 +0800

    alteer

--- run.py
+++ run.py
@@ -2,8 +2,10 @@
 import sys
 from wxcloudrun import app
 
-if len(sys.argv) < 3:
-    sys.argv = [sys.argv[0], '127.0.0.1', '8000']
+# local test
+# if len(sys.argv) < 3:
+#     sys.argv = [sys.argv[0], '127.0.0.1', '8000']
+
 
 # 启动Flask Web服务
 if __name__ == '__main__':
