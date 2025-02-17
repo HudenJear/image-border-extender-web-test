199188452762c3c1caaf37976fe71788f0348431
Zheng Gong <hudenjear@foxmail.com>
2025-02-17 23:47:18 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 import sys
 from wxcloudrun import app
 
-# local test
-if len(sys.argv) < 3:
-    sys.argv = [sys.argv[0], '192.168.50.37', '5000']
+# # local test
+# if len(sys.argv) < 3:
+#     sys.argv = [sys.argv[0], '192.168.50.37', '5000']
 
 
 # 启动Flask Web服务
